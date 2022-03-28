0f6710d5d63d628c6cf921ffbb31bde80935ab5a
BrendenSeifried <brendenseifried@gmail.com>
2022-03-28 16:19:15 -0700

    added all info needed to send to supabase

--- src/components/AddDog.js
+++ src/components/AddDog.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { createDog } from '../services/fetchdogs';
 
-export default function AddDog({ name, setName, age, setAge }) {
+export default function AddDog({ name, setName, age, setAge, bio, setBio, breed, setBreed, image, setImage }) {
   const [error, setError] = useState('');
   const submitDog = async () => {
     try {
-      const submit = await createDog({ name, age });
+      const submit = await createDog({ name, age, bio, breed, image });
       console.log(submit);
     } catch (e) {
       setError('Oh no!');
@@ -15,8 +15,28 @@ export default function AddDog({ name, setName, age, setAge }) {
   return (
     <div>
       {error && <p>{error}</p>}
-      <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
-      <input type="number" value={age} onChange={(e) => setAge(e.target.value)}/>
+
+
+      <label> Link a Photo:
+        <input type="text" value={image} onChange={(e) => setImage(e.target.value)}/>
+      </label>
+
+      <label> Name:
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+      </label>
+
+      <label> Age:
+        <input type="number" value={age} onChange={(e) => setAge(e.target.value)}/>
+      </label>
+
+      <label> Background:
+        <input type="text" value={bio} onChange={(e) => setBio(e.target.value)}/>
+      </label>
+
+      <label> Breed:
+        <input type="text" value={breed} onChange={(e) => setBreed(e.target.value)}/>
+      </label>
+
       <button onClick={submitDog}>submit</button>
     </div>
   );
--- src/views/NewDog/NewDog.js
+++ src/views/NewDog/NewDog.js
@@ -4,10 +4,13 @@ import AddDog from '../../components/AddDog';
 export default function Admin() {
   const [name, setName] = useState('');
   const [age, setAge] = useState(null);
+  const [bio, setBio] = useState('');
+  const [breed, setBreed] = useState('');
+  const [image, setImage] = useState('');
 
   return (
     <div>
-      <AddDog {...{ name, age, setName, setAge }}/>
+      <AddDog {...{ name, age, setName, setAge, bio, setBio, breed, setBreed, image, setImage }}/>
     </div>
   );
 }
