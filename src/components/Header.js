a16c967ea986da2979b1854255ba99dee70fb222
BrendenSeifried <brendenseifried@gmail.com>
2022-03-30 13:44:34 -0700

    made footer the header just need to css the logout button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,35 @@
+// // import React from 'react';
+// // import { NavLink } from 'react-router-dom';
+
+
+
+// export default function Header({ currentUser, setCurrentUser }) {
+//   const handleLogout = async () => {
+//     await logout();
+//     setCurrentUser('');
+//   };
+
+//   return (
+//     <div>
+//       {!currentUser && 
+//         <div onClick={handleLogout}>
+//           <button>logout</button>
+            
+//         </div>
+        
+//       }
+//       {/* <NavLink className='nav' exact to = '/'>
+//         <h1>See all dogs</h1>
+//       </NavLink> */}
+
+    
+//     </div>
+//   );
+// }
+
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Footer.css';
 import { logout } from '../services/fetchauth';
 
 
@@ -9,21 +39,26 @@ export default function Header({ currentUser, setCurrentUser }) {
     await logout();
     setCurrentUser('');
   };
-
   return (
-    <div>
+    <div className='navlinkdiv'>
       {!currentUser && 
-        <div onClick={handleLogout}>
+        <div className='nav' onClick={handleLogout}>
           <button>logout</button>
             
         </div>
-        
       }
-      {/* <NavLink className='nav' exact to = '/'>
-        <h1>See all dogs</h1>
-      </NavLink> */}
+        
 
-    
+      <NavLink className='nav' exact to = '/'>
+        <h1>See all dogs</h1>
+      </NavLink>
+
+      <NavLink className='nav' exact to = '/dogs/new'>
+        <h1>Admin Page</h1>
+      </NavLink>
+
+     
+      
     </div>
   );
 }
